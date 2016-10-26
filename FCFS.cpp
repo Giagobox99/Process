4922448b2e0ffc5fd3ec13a357b37e8bf7933d52
Giagobox99 <jacopo.mereu@tiscali.it>
2016-10-26 13:18:15 +0200

    Update FCFS.cpp

--- FCFS.cpp
+++ FCFS.cpp
@@ -2,33 +2,35 @@
 
 int main()
 {
-	int processCount,i;
-	std::cout<<"This programm emulate the algorithm of FCFS "<<std::endl<<std::endl;
-	std::cout<<"Insert the number of processes "<<std::endl;
-	std::cin>>processCount;
+	int processCount, i;
+	std::cout << "This programm emulate the algorithm of FCFS " << std::endl << std::endl;
+	std::cout << "Insert the number of processes " << std::endl;
+	std::cin >> processCount;
 	
 	int burstTime[processCount], waitingTime[processCount], tournaroundTime[processCount];
+	double avgWaitingTime = 0; avgTour
 	
 	for (i=0; i<processCount; i++)
 	{
-		std::cout<<"Insert the ("<<i+1<<") input of BurstTime"<<std::endl;
-		std::cin>>burstTime[i];
+		std::cout << "Insert the (" << i+1 << ") input of BurstTime" << std::endl;
+		std::cin >> burstTime[i];
 		waitingTime[0]=0;
-		if(i>0)
+		if (i>0)
 		{
-			waitingTime[i]=waitingTime[i-1]+burstTime[i-1];
+			waitingTime[i] = waitingTime[i-1] + burstTime[i-1];
 		}
-		tournaroundTime[i]=waitingTime[i]+burstTime[i];
+		tournaroundTime[i] = waitingTime[i] + burstTime[i];
+		avgWaitingTime += waitingTime[i]
 	
 	}
 	for (i=0; i<processCount; i++)
 	{
-		std::cout<<"Il ("<<i+1<<") waitingTime vale ("<<waitingTime[i]<<")"<<std::endl;
+		std::cout << "Il (" << i+1 << ") waitingTime vale (" << waitingTime[i] << ")" << std::endl;
 	}
-		for(i=0;i<processCount;i++)
+		for (i=0; i<processCount; i++)
 	{
-		std::cout<<"Il ("<<i+1<<") tournaroundTime vale ("<<tournaroundTime[i]<<")"<<std::endl;
+		std::cout << "Il (" << i+1 << ") tournaroundTime vale (" << tournaroundTime[i] << ")" << std::endl;
 	}
 	
-	system("pause");
+	system ("pause");
 }
